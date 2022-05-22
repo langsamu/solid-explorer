814cc8426b1acffaaf136050b091bea3ecb284e3
Samu Lang <langsamu@hotmail.com>
2022-05-22 14:59:18 +0100

    Upgrade N3

--- scripts/SolidClient.js
+++ scripts/SolidClient.js
@@ -1,4 +1,4 @@
-import "https://unpkg.com/n3@1.14.0/browser/n3.min.js"
+import "https://unpkg.com/n3@1.16.2/browser/n3.min.js"
 import "https://unpkg.com/jsonld@5.2.0/dist/jsonld.esm.min.js"
 import {UmaClient} from "./UmaClient.js"
 import {HttpHeader, HttpMethod, Ldp, Mime, Solid} from "./Vocabulary.js"
